b1c42451224ef67c931584bdc501c02a85a4ea87
Harsh raj <harshrj9939@gmail.com>
2024-07-06 09:58:10 +0530

    commit

--- src/Api/api.js
+++ src/Api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: 'https://openlibrary.org/search.json?q=YOUR_QUERY&limit=10&page=1',
 });
 
-export const searchBooks = async (query, limit = 10, page = 1) => {
+export const searchBooks = async (query, limit = 1, page = 1) => {
     try {
         const response = await api.get(`search.json?q=${query}&limit=${limit}&page=${page}`);
         return response.data;
